94bbf669dc5129ea21b4f0b6b77f3c1b5dc9e8dd
king evan <kingevan1@jaynes-macbook-pro.local>
2018-09-30 15:03:57 -0400

    Initial Commit

---
+++ AR.BESTMAGIC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR.BESTMAGIC
+//
+//  Created by King Evan on 9/30/18.
+//  Copyright © 2018 KE INC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
